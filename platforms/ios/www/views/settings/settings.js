3fd305e5f6d3d0e51c68cd5dcdd711bc8bdc44f9
vaibhav samadhiya <vaibhav.samadhiya@gmail.com>
2017-01-21 18:14:20 +1100

    Removing all files in .gitignore

--- platforms/ios/www/views/settings/settings.js
+++
@@ -1,9 +0,0 @@
-angular.module('App')
-  .controller('SettingsController', function ($scope, Settings, Fav) {
-    $scope.settings = Settings;
-    $scope.events = Fav.data;
-    $scope.canDelete = false;
-    $scope.remove = function (index) {
-      Fav.remove(Fav.data[index]);
-    };
-  });
